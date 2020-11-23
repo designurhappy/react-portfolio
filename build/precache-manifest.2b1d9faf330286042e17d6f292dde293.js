ded500f21fe5e0ff49f97e4b5be564f56333bdde
Moo Point <beachgal0105@gmail.com>
2020-11-22 19:15:43 -0800

    issues with deployment url

---
+++ build/precache-manifest.2b1d9faf330286042e17d6f292dde293.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f060b6eddc3367fe60267daf79350b08",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "3af5482410f3974147bc",
+    "url": "/react-portfolio/static/css/main.e3686ae9.chunk.css"
+  },
+  {
+    "revision": "47385e60a366969e4c1e",
+    "url": "/react-portfolio/static/js/2.f1771c48.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/react-portfolio/static/js/2.f1771c48.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3af5482410f3974147bc",
+    "url": "/react-portfolio/static/js/main.9b3bf05d.chunk.js"
+  },
+  {
+    "revision": "aff33cef9dbb8787d6b3",
+    "url": "/react-portfolio/static/js/runtime-main.213b1880.js"
+  },
+  {
+    "revision": "7466cd9cd329f71cd5f7fcc2ebb20af7",
+    "url": "/react-portfolio/static/media/ANR.7466cd9c.jpg"
+  },
+  {
+    "revision": "3766f4838cc156ed8a63f3dfaaaac15a",
+    "url": "/react-portfolio/static/media/AliciaRoss_Res.3766f483.pdf"
+  },
+  {
+    "revision": "6acf9b3281311e37de1c32241b932511",
+    "url": "/react-portfolio/static/media/Covie.6acf9b32.jpg"
+  },
+  {
+    "revision": "8ca1a23e68833e7fd28ec0c6dbde1935",
+    "url": "/react-portfolio/static/media/Shrinko.8ca1a23e.jpg"
+  },
+  {
+    "revision": "e728dcda6a9f2eb0dc7e7f056ca9c0bd",
+    "url": "/react-portfolio/static/media/run-buddy.e728dcda.jpg"
+  }
+]);
